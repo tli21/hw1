9ba75486488b3202bcd5f10660357136dadea9d2
tli21 <54990201+tli21@users.noreply.github.com>
2019-09-08 11:06:37 -0400

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,17 @@
+#Ting
+#September 6
+#A Conversation
+#Soures: None
+#OMH
+
+print("Hi user\n")
+
+user_response = input("How are you today?\n")
+
+user_response = input("That's cool. What did you eat for breakfast?\n")
+
+user_reponse = input("That's tasty. What do you like to do?\n")
+
+user_reponse = input("Awesome! What are you planning to do today?\n")
+
+print("Have a good day!")
